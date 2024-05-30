855c703b2409c979bbeb8a474c6175c68b65ac00
Tim Smith <2762951+tahsmith@users.noreply.github.com>
2024-05-30 01:36:18 +0000

    go back to cp

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 # Path to this file
 repo_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-rsync -av --exclude=".git*" ${repo_root}/ ~
+pushd $repo_root
+# rsync -av --exclude=".git*" ./ ~
+cp -r ./ ~
+popd
 
 # Install plugins
 nvim --headless "+Lazy! sync" +qa
